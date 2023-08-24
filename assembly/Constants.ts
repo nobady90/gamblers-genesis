e729c1c332f8e49044866f7f5dc59fdae01488b3
lomba <f.lombardini@open-box.it>
2023-08-24 16:45:14 +0200

    update Constants.ts.

--- assembly/Constants.ts
+++ assembly/Constants.ts
@@ -1,11 +1,11 @@
 import { Base58 } from "@koinos/sdk-as";
 
 export namespace Constants {
-  export const NAME: string = "";
-  export const SYMBOL: string = "";
+  export const NAME: string = "Gamblers Genesis";
+  export const SYMBOL: string = "KRGG";
   export const MINT_PRICE: u64 = 0;
   export const MINT_FEE: bool = false;
-  export const MAX_SUPPLY: u64 = 100000000000000;
+  export const MAX_SUPPLY: u64 = 50;
   export const URI: string = "";
   export const OWNER: Uint8Array = Base58.decode("");
 
